3299c309613a45f4356f9b915d27f7bea6f9a04b
Joshua Smart <josh@thesmarts.co.uk>
2021-10-25 22:00:10 +0100

    Implemented main class initiation

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+import Model from './model/model';
+import View from './view/view';
+import Controller from './controller/controller';
+
+const model = new Model();
+const view = new View(model);
+new Controller(model, view);
