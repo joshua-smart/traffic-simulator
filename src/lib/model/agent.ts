db59411fa93e694974f942244605afa838eeff79
Joshua Smart <josh@thesmarts.co.uk>
2021-10-26 18:56:12 +0100

    Implemented Djikstra's algrithm for Stack based pathfinding

--- src/lib/model/agent.ts
+++ src/lib/model/agent.ts
@@ -1,7 +1,7 @@
-import Queue from '../queue';
+import Stack from '../stack';
 
 export default class Agent {
     private distance: number;
     private speed: number;
-    private route: Queue<number>;
+    private route: Stack<number>;
 }
--- src/lib/model/roadNetwork.ts
+++ src/lib/model/roadNetwork.ts
@@ -1,11 +1,61 @@
 import Graph from './graph';
 import Vector2 from '../vector2';
+import Stack from '../stack';
 
 type Vertex = Vector2;
 type Edge = {srcTangent: Vector2, dstTangent: Vector2};
+type Edge = {p1: Vector2, p2: Vector2};
 
 export default class RoadNetwork extends Graph<Vertex, Edge>{
     constructor() {
         super();
     }
+
+    private get_edge_length(srcId: number, dstId: number): number {
+        const srcV = this.get_vertex(srcId);
+        const dstV = this.get_vertex(dstId);
+        return Vector2.sub(srcV, dstV).magnitude();
+    }
+
+    public find_route(srcId: number, dstId: number): Stack<number> {
+        const q: number[] = [];
+        const dist: number[] = [];
+        const prev: number[] = [];
+
+        for(let v = 0; v < this.get_size(); v++) {
+            dist[v] = Infinity;
+            prev[v] = undefined;
+            q.push(v);
+        }
+        dist[srcId] = 0;
+
+        while (q.length > 0) {
+            q.sort((a, b) => dist[a] - dist[b]);
+            const u = q.shift();
+
+            if (u == dstId) {
+                return;
+            }
+
+            for(let v = 0; v < this.get_size(); v++) {
+                if(this.get_edge(u, v) && q.includes(v)) {
+                    const alt = dist[u] + this.get_edge_length(u, v);
+                    if (alt < dist[v]) {
+                        dist[v] = alt;
+                        prev[v] = u;
+                    }
+                }
+            }
+        }
+
+        const s: Stack<number> = new Stack<number>();
+        let u = dstId;
+
+        while (u) {
+            s.push(u);
+            u = prev[u];
+        }
+        return s;
+    }
+}
 }
