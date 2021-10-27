50569e96a9f94a4e233649ad333c0c40bae4e5e3
Joshua Smart <josh@thesmarts.co.uk>
2021-10-27 20:32:25 +0100

    Commented CubicBezier class

--- src/lib/view/cubicBezier.ts
+++ src/lib/view/cubicBezier.ts
@@ -1,5 +1,6 @@
 import Vector2 from '../vector2';
 
+// Resolution used to generate the distanceLookup table
 const interpolationResolution = 0.01;
 
 export default class CubicBezier {
@@ -11,11 +12,13 @@ export default class CubicBezier {
         this.distanceLookup = this.generate_lookup();
     }
 
+    // Ensure the distanceLookup is re-generated every time the curve is changed
     public set_vertex(index: number, value: Vector2): void {
         this.vertices[index] = value;
         this.distanceLookup = this.generate_lookup();
     }
 
+    // Implementation of cubic bezier curve equation with respect to parameter t
     private interpolate(t: number): Vector2 {
         return new Vector2(0, 0).add(
             this.vertices[0].mult((1 - t)**3)
@@ -28,6 +31,7 @@ export default class CubicBezier {
         );
     }
 
+    // Implementation of cubic bezier derivative with respect to parameter t
     private tangent(t: number): Vector2 {
         return new Vector2(0, 0).add(
             this.vertices[0].mult(-3*t**2 + 6*t - 3)
@@ -40,6 +44,7 @@ export default class CubicBezier {
         );
     }
 
+    // Create distanceLookup by cumulatively evaluating small straight line distances
     private generate_lookup(): Map<number, number> {
         const map = new Map<number, number>();
         let distance = 0;
@@ -49,11 +54,13 @@ export default class CubicBezier {
             const p = this.interpolate(t);
             t += interpolationResolution;
             const q = this.interpolate(t);
+            // Straight line distance from p to q
             distance += Vector2.sub(p, q).magnitude();
         }
         return map;
     }
 
+    // Find first t such that distance is greater than the target, else return null
     public get_t_at_distance(targetDistance: number): number {
         for(const [t, distance] of this.distanceLookup) {
             if (distance >= targetDistance) return t;
@@ -61,11 +68,14 @@ export default class CubicBezier {
         return null;
     }
 
+    // Find t then evaluate using interpolation function
     public get_point_at_distance(targetDistance: number) {
         const t = this.get_t_at_distance(targetDistance);
+        if (t === null) return null;
         return this.interpolate(t);
     }
 
+    // Find t then evaluate using tangent function
     public get_tangent_at_distance(targetDistance: number) {
         const t = this.get_t_at_distance(targetDistance);
         if (t === null) return null;
