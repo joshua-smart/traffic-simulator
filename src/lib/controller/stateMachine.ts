582f2eb9f01b17479d9aabbb68795f2cd98e5f16
Joshua Smart <josh@thesmarts.co.uk>
2021-10-27 20:37:28 +0100

    Commented StateMachine class

--- src/lib/controller/stateMachine.ts
+++ src/lib/controller/stateMachine.ts
@@ -7,16 +7,18 @@ export default class StateMachine<Payload> {
         this.currentStateId = initalStateId;
     }
 
+    // Use the cantor pairing function to map every pair of integers (stateId, eventId) to a unique value (transitionId) en.wikipedia.org/wiki/Pairing_function#Cantor_pairing_function
     private static get_transition_id(stateId: number, eventId: number) {
-        // This uses the cantor pairing function to map every pair of integers to unique values en.wikipedia.org/wiki/Pairing_function#Cantor_pairing_function
         return 0.5 * (stateId + eventId) * (stateId + eventId + 1) + eventId;
     }
 
+    // Generate transitionId for state and event combo and add transition data to transitions Map
     public add_rule(previousStateId: number, eventId: number, newStateId: number, callback: (payload: Payload) => void) {
         const transitionId = StateMachine.get_transition_id(previousStateId, eventId);
         this.transitions.set(transitionId, {newStateId, callback});
     }
 
+    // If the transition exists, update state to newState and call callback, passing payload if specified
     public transition(eventId: number, payload: Payload) {
         const transitionId = StateMachine.get_transition_id(this.currentStateId, eventId);
         if(!this.transitions.has(transitionId)) return;
