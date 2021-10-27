ac0b34dc20e8f91ce41c1c8560066c6c74186ddb
Joshua Smart <josh@thesmarts.co.uk>
2021-10-27 20:22:34 +0100

    Commented Graph class

--- src/lib/model/graph.ts
+++ src/lib/model/graph.ts
@@ -6,17 +6,20 @@ export default class Graph<Vertex, Edge> {
     constructor() {
         this.adjacencyMatrix = [];
         this.vertices = [];
+        // Constant for empty edges declared here for type consistency
         this.empty = <Edge><unknown>null;
     }
 
-    public get_size() {
+    public size() {
         return this.vertices.length;
     }
 
+    // Adds vertex to vertices array and ads a row and column to adjacencyMatrix
     public add_vertex(vertex: Vertex): number {
         this.vertices.push(vertex);
         this.adjacencyMatrix.forEach(row => row.push(this.empty));
         this.adjacencyMatrix.push(new Array(this.vertices.length));
+        // Return index of added vertex
         return this.vertices.length - 1;
     }
 
@@ -30,6 +33,7 @@ export default class Graph<Vertex, Edge> {
         return this.vertices[vertexId];
     }
 
+    // Removes vertex from vertex array and removes it's row and column from adjacencyMatrix
     public remove_vertex(vertexId: number): Vertex {
         this.check_vertex_index(vertexId);
         const removedVertex = this.vertices[vertexId];
@@ -55,12 +59,14 @@ export default class Graph<Vertex, Edge> {
         return removedEdge;
     }
 
+    // Internally used check for valid vertexId
     private check_vertex_index(vertexId: number): void {
         if(vertexId < 0 || vertexId >= this.vertices.length) {
             throw new Error(`vertexId (${vertexId}) out of range for length (${this.vertices.length})`);
         }
     }
 
+    // Internally used check for valid edge location
     private check_edge_index(srcId: number, dstId: number): void {
         if(srcId < 0 || srcId >= this.vertices.length || dstId < 0 || dstId >= this.vertices.length) {
             throw new Error(`srcId (${srcId}), dstId (${dstId}) out of range for length (${this.vertices.length})`);
