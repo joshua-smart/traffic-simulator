bfdd9c6d0ff55abee06db0fc51bcd83cdcca334d
Joshua Smart <josh@thesmarts.co.uk>
2021-10-30 18:20:58 +0100

    Commented ioManager class

--- src/lib/controller/ioManager.ts
+++ src/lib/controller/ioManager.ts
@@ -2,6 +2,7 @@ import RoadNetwork from "../model/roadNetwork";
 import { saveAs } from 'file-saver';
 import Vector2 from "../vector2";
 
+// Specifies the objects that must be contained in the json result
 type JSONRoadNetwork = {
     adjacencyMatrix: {
         p1: {x: number, y: number}, p2: {x: number, y: number}
@@ -10,20 +11,26 @@ type JSONRoadNetwork = {
 }
 
 export default class IOManager {
+
+    // Save current road network to a file
     public static save_road_network(roadNetwork: RoadNetwork): void {
+        // Convert roadNetwork to json representation
         const json = JSON.stringify(roadNetwork)
         var file = new File([json], 'my road network.json', {type: 'application/json;charset=utf-8'});
         saveAs(file);
     }
 
+    // Request file from the user and load it into the current roadNetwork
     public static async load_road_network(callback: (roadNetwork: RoadNetwork) => void) {
         const fileInput = <HTMLInputElement>document.querySelector('#file-input')
         fileInput.click();
         fileInput.onchange = async () => {
+            // Catch errors on invalid files
             try {
                 const file = fileInput.files[0];
+                // Convert text to JSON
                 const json = <JSONRoadNetwork>JSON.parse(await file.text());
-
+                // Convert json to RoadNetwork
                 const roadNetwork = this.json_to_road_network(json);
                 callback(roadNetwork);
             } catch {
@@ -32,15 +39,18 @@ export default class IOManager {
         }
     }
 
+    // Apply data in JSON object to a blank RoadNetwork and return
     private static json_to_road_network(json: JSONRoadNetwork) {
         const roadNetwork = new RoadNetwork();
 
+        // Set vertices
         for(let vertexId = 0; vertexId < json.vertices.length; vertexId++) {
             const jsonVertex = json.vertices[vertexId];
             const vertex = new Vector2(jsonVertex.x, jsonVertex.y);
             roadNetwork.add_vertex(vertex);
         }
 
+        // Set edges
         for(let srcId = 0; srcId < roadNetwork.size(); srcId++) {
             for(let dstId = 0; dstId < roadNetwork.size(); dstId++) {
                 const jsonEdge = json.adjacencyMatrix[srcId][dstId];
