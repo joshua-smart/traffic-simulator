25a69337e5e42551142feea69f8d80095a4ace35
Joshua Smart <josh@thesmarts.co.uk>
2021-10-30 13:54:21 +0100

    Installed source-map-loader, fixed source map bug

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -10,6 +10,11 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader']
+      },
     ],
   },
   resolve: {
