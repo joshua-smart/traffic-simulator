aec848177cd949e05044730ba49af4a3daa29d9c
Joshua Smart <josh@thesmarts.co.uk>
2021-10-30 12:09:16 +0100

    Removed canvas attribute from roadnetwork painter

--- src/lib/view/roadNetworkPainter.ts
+++ src/lib/view/roadNetworkPainter.ts
@@ -10,29 +10,25 @@ export type GhostEdge = {
 };
 
 export default class RoadNetworkPainter {
-    private canvas: Canvas;
     private vertexContainer: HTMLElement;
     private ghostEdge: GhostEdge;
 
-    constructor(canvas: Canvas) {
-        this.canvas = canvas;
-
+    constructor() {
         this.ghostEdge = null;
         this.vertexContainer = document.querySelector('#vertex-container');
     }
 
-    public draw(roadNetwork: RoadNetwork, transform: Transform): void {
-        this.canvas.clear();
-        if (this.ghostEdge) this.draw_ghost_edge(roadNetwork, transform);
-        this.draw_edges(roadNetwork, transform);
+    public draw(canvas: Canvas, roadNetwork: RoadNetwork, transform: Transform): void {
+        if (this.ghostEdge) this.draw_ghost_edge(canvas, roadNetwork, transform);
+        this.draw_edges(canvas, roadNetwork, transform);
         this.draw_vertices(roadNetwork, transform);
     }
 
-    private draw_ghost_edge(roadNetwork: RoadNetwork, transform: Transform) {
+    private draw_ghost_edge(canvas: Canvas, roadNetwork: RoadNetwork, transform: Transform) {
         const srcWorldPosition = roadNetwork.get_vertex(this.ghostEdge.srcId);
         const srcScreenPosition = transform.to_screen_space(srcWorldPosition);
 
-        this.canvas.line(srcScreenPosition, this.ghostEdge.end, {color: 'lightgrey', width: 10 * transform.get_scale(), cap: 'round'});
+        canvas.line(srcScreenPosition, this.ghostEdge.end, {color: 'lightgrey', width: 10 * transform.get_scale(), cap: 'round'});
     }
 
     private draw_vertices(roadNetwork: RoadNetwork, transform: Transform): void {
@@ -56,16 +52,16 @@ export default class RoadNetworkPainter {
         this.vertexContainer.appendChild(vertexElement);
     }
 
-    private draw_edges(roadNetwork: RoadNetwork, transform: Transform): void {
+    private draw_edges(canvas: Canvas, roadNetwork: RoadNetwork, transform: Transform): void {
 
         for(let srcId = 0; srcId < roadNetwork.size(); srcId++) {
             for(let dstId = 0; dstId < roadNetwork.size(); dstId++) {
-                this.draw_edge(roadNetwork, srcId, dstId, transform);
+                this.draw_edge(canvas, roadNetwork, srcId, dstId, transform);
             }
         }
     }
 
-    private draw_edge(roadNetwork: RoadNetwork, srcId: number, dstId: number, transform: Transform): void {
+    private draw_edge(canvas: Canvas, roadNetwork: RoadNetwork, srcId: number, dstId: number, transform: Transform): void {
         const edge = roadNetwork.get_edge(srcId, dstId);
         if (!edge) return;
 
@@ -79,7 +75,7 @@ export default class RoadNetworkPainter {
         const screenVertices = worldVertices.map((vertex) => transform.to_screen_space(vertex));
 
         const bezier = new CubicBezier(screenVertices[0], screenVertices[1], screenVertices[2], screenVertices[3]);
-        this.canvas.bezier(bezier,
+        canvas.bezier(bezier,
             {color: 'lightgrey', width: 10 * transform.get_scale(), cap: 'round'},
             {color: 'grey', line: {color: 'transparent', width: 1}}
         );
