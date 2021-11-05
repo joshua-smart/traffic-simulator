9706f7715c290517078a8523584af5c8c795fb14
Joshly3 <josh@thesmarts.co.uk>
2021-11-05 18:25:03 +0000

    Added methods to Agent class

--- src/lib/model/agent.ts
+++ src/lib/model/agent.ts
@@ -4,4 +4,30 @@ export default class Agent {
     private distance: number;
     private speed: number;
     private route: Stack<number>;
+    private currentSrcVertex: number;
+
+    private acceleration: number;
+
+    constructor(route: Stack<number>) {
+        this.route = route;
+        this.currentSrcVertex = this.route.pop();
+        this.distance = 0;
+    }
+
+    public move_to_next_edge(): void {
+        this.currentSrcVertex = this.route.pop();
+        this.distance = 0;
+    }
+
+    public get_edge(): {srcId: number, dstId: number} {
+        return {
+            srcId: this.currentSrcVertex,
+            dstId: this.route.peek()
+        }
+    }
+
+    public increment_position(): void {
+        this.speed += this.acceleration;
+        this.distance += this.speed;
+    }
 }
