28f4fb05c28cc6be0f1c147e5f894d7bb86df3eb
Joshua Smart <josh@thesmarts.co.uk>
2021-11-25 19:02:30 +0000

    Commented Stack class

--- src/lib/stack.ts
+++ src/lib/stack.ts
@@ -7,27 +7,32 @@ export default class Stack<Element> {
         this.length = 0;
     }
 
+    // Set element at index = length and increment length
     public push(element: Element): void {
         this.elements[this.length] = element;
         this.length++;
     }
 
+    // Get top element and decrement length
     public pop(): Element {
         const topElement = this.peek();
         this.length--;
         return topElement;
     }
 
+    // If stack currently has no elements
     public is_empty(): boolean {
         return this.length === 0;
     }
 
+    // Pop all elements from the stack
     public clear(): void {
         while(!this.is_empty()) {
             this.pop();
         }
     }
 
+    // Get top element of stack
     public peek(): Element {
         return this.elements[this.length - 1];
     }
