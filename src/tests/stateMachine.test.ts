7d112a6851efff73d7fdf0c0fc3af28e52020d17
Joshua Smart <josh@thesmarts.co.uk>
2021-12-12 14:32:36 +0000

    Added StateMachine test suite

---
+++ src/tests/stateMachine.test.ts
@@ -0,0 +1,21 @@
+import StateMachine from "../lib/controller/stateMachine";
+
+describe("state machine test suite", () => {
+    let stateMachine: StateMachine<number>;
+
+    it("instantiate state machine class", () => {
+        stateMachine = new StateMachine<number>(0);
+    });
+
+    it("add rule (0, 0) -> (1, () => {})", () => {
+        stateMachine.add_rule(0, 0, 1, () => {});
+    });
+
+    it("transition with event 0", () => {
+        stateMachine.transition(0, null);
+    });
+
+    it("transition with unbound event 1", () => {
+        stateMachine.transition(1, null);
+    });
+});
