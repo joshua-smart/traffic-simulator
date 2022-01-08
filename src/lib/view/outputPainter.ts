577ec1eb0edb0057bb3811c1faa1a3eb6eed5939
Joshua Smart <josh@thesmarts.co.uk>
2022-01-08 14:04:39 +0000

    Updated output units

--- src/lib/view/outputPainter.ts
+++ src/lib/view/outputPainter.ts
@@ -7,20 +7,22 @@ export default class OutputPainter {
     constructor() {
         this.formats = {
             'agentCount': (input) => `${input}`,
-            'simTimer': (input) => `${(input / 1000).toFixed(2)}s`,
-            'avgAliveTime': (input) => `${(input / 1000).toFixed(2)}s`,
-            'avgDistance': (input) => `${input.toFixed(2)}m`,
-            'avgMaxSpeed': (input) => `${input.toFixed(2)}mps`,
-            'avgMinSpeed': (input) => `${input.toFixed(2)}mps`,
-            'avgStopTime': (input) => `${(input / 1000).toFixed(2)}s`
+            'simTimer': (input) => `${(input / 1000).toFixed(2)}<span class="text-sm">s</span>`,
+            'avgAliveTime': (input) => `${(input / 1000).toFixed(2)}<span class="text-sm">s</span>`,
+            'avgDistance': (input) => `${input.toFixed(2)}<span class="text-sm">m</span>`,
+            'avgMaxSpeed': (input) => `${input.toFixed(2)}<span class="text-sm">ms<sup>-1</sup></span>`,
+            'avgMinSpeed': (input) => `${input.toFixed(2)}<span class="text-sm">ms<sup>-1</sup></span>`,
+            'avgStopTime': (input) => `${(input / 1000).toFixed(2)}<span class="text-sm">s</span>`
         };
     }
 
     public draw(simulationOutput: SimulationOutput): void {
         Object.keys(simulationOutput).forEach(id => {
             const element = document.querySelector(`#${id}`);
-            const value = this.formats[id](simulationOutput[id]);
-            element.innerHTML = `${value}`;
+            const value = simulationOutput[id];
+            if (isNaN(value)) return;
+            const text = this.formats[id](value);
+            element.innerHTML = `${text}`;
         });
     }
 }
