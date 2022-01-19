2df718c82f33b5fd6ea8e38ca52ce0be7803fb96
Joshly3 <josh@thesmarts.co.uk>
2022-01-19 10:07:12 +0000

    Updated webpack config

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, './build'),
   },
 };
